34910d8da5f1b429d0eb41b7788ed7dd459a33a4
alisheyev <davronbekalisheyev02@gmail.com>
2023-02-25 20:12:08 +0500

    best section TUGATTIM

--- js/owl.responsive.js
+++ js/owl.responsive.js
@@ -1,22 +1,36 @@
-$("#owl-carousel_section .owl-carousel").owlCarousel({  //id section name
+$("#best-section .owl-carousel").owlCarousel({  //id section name
   loop: true, // "true" is infinity - "false" is not infinity
   dots: true, // "true" is dots - "false" is not dots
-  nav: true,  // "true" is navigation - "false" is not navigation
+  nav: false,  // "true" is navigation - "false" is not navigation
   margin: 10, //item margin
   responsive: {
     0: {
       items: 1,
-      nav: true,
+      nav: false,
       loop: true,
     },
-    600: {
+    550: {
       items: 2,
-      nav: true,
+      nav: false,
       loop: true,
     },
-    1000: {
-      items: 3,
-      nav: true,
+  },
+});
+
+$("#catigory-section .owl-carousel").owlCarousel({  //id section name
+  loop: true, // "true" is infinity - "false" is not infinity
+  dots: true, // "true" is dots - "false" is not dots
+  nav: false,  // "true" is navigation - "false" is not navigation
+  margin: 10, //item margin
+  responsive: {
+    0: {
+      items: 2,
+      nav: false,
+      loop: true,
+    },
+    550: {
+      items: 2,
+      nav: false,
       loop: true,
     },
   },
